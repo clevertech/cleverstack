506bc11cafbf1f43cf65e7758b9583898197aca8
Sam Deering <sam@clevertech.biz>
2013-12-02 14:25:41 +0000

    deps fix

--- cleverstack-node-seed/index.js
+++ cleverstack-node-seed/index.js
@@ -1,19 +1,19 @@
 // Get everything ready
-var config = require( './config' )
-  , express = require( 'express' )
-  , cors = require( 'cors' )
-  , webPort = process.env.NODE_WWW_PORT || config.webPort || 8080
-  , env = process.env.NODE_ENV || config.environmentName || 'development'
-  , initializeRoutes = require( './routes' )
-  , modelInjector = require( 'utils' ).modelInjector
-  , Sequelize = require( 'sequelize' )
-  , Injector = require( 'utils' ).injector
-  , passport = require( 'passport' )
-  , mongoose = require( 'mongoose' )
-  , initializeSecurity = require( './security' )
-  , app = express();
+var config = require('./config'),
+    express = require('express'),
+    cors = require('cors'),
+    webPort = process.env.NODE_WWW_PORT || config.webPort || 8080,
+    env = process.env.NODE_ENV || config.environmentName || 'development',
+    initializeRoutes = require('./routes'),
+    modelInjector = require('utils').modelInjector,
+    Sequelize = require('sequelize'),
+    Injector = require('utils').injector,
+    passport = require('passport'),
+    mongoose = require('mongoose'),
+    initializeSecurity = require('./security'),
+    app = express();
 
-var RedisStore = require( 'connect-redis' )( express );
+var RedisStore = require('connect-redis')(express);
 
 // Setup ORM
 var sequelize = new Sequelize(
@@ -24,80 +24,83 @@ var sequelize = new Sequelize(
 );
 
 // Setup ODM
-if ( config.odm && config.odm.enabled ) {
-  mongoose.connect(config.mongoose.uri);
+if (config.odm && config.odm.enabled) {
+    mongoose.connect(config.mongoose.uri);
 }
 
 // Bootstrap our DI
-GLOBAL.injector = Injector(  __dirname + '/src/services/', __dirname + '/src/controllers/' );
+GLOBAL.injector = Injector(__dirname + '/src/services/', __dirname + '/src/controllers/');
 
-app.set( 'port', webPort );
-app.set( 'injector', injector );
+app.set('port', webPort);
+app.set('injector', injector);
 
-injector.instance( 'sequelize', sequelize );
-injector.instance( 'config', config );
-injector.instance( 'mongoose', mongoose );
-injector.instance( 'db', sequelize );
+injector.instance('sequelize', sequelize);
+injector.instance('config', config);
+injector.instance('mongoose', mongoose);
+injector.instance('db', sequelize);
 
 // Get our models
-var models = require( 'models' )
-injector.instance( 'models', models );
+var models = require('models')
+injector.instance('models', models);
 
 // Run our model injection service
-modelInjector( models );
+modelInjector(models);
 
 app.configure(function() {
 
     // static file delivery
-    app.use( express[ 'static' ]( __dirname + '/public' ) );
+    app.use(express['static'](__dirname + '/public'));
 
     // app.use( cors() ); // automatically supports pre-flighting
-    app.use( cors( {
-        origin: true
-        , credentials:true
-        , headers: ["x-requested-with", "content-type"]
-        , methods: ["GET", "PUT", "POST", "DELETE", "OPTIONS"]
-        , maxAge:1000000000
+    app.use(cors({
+        origin: true,
+        credentials: true,
+        headers: ["x-requested-with", "content-type"],
+        methods: ["GET", "PUT", "POST", "DELETE", "OPTIONS"],
+        maxAge: 1000000000
     }));
 
     // middleware stack
-    app.use( express.bodyParser() );
+    app.use(express.bodyParser());
 
     // session management
-    app.use( express.cookieParser() );
-    app.use( express.session({
-        secret: config.secretKey
-        , cookie: { secure: false, maxAge: 86400000 }
-        , store: new RedisStore({
-            host: config.redis.host
-            , port: config.redis.port
-            , prefix: config.redis.prefix+process.env.NODE_ENV+"_"
-            , password: config.redis.key
+    app.use(express.cookieParser());
+    app.use(express.session({
+        secret: config.secretKey,
+        cookie: {
+            secure: false,
+            maxAge: 86400000
+        },
+        store: new RedisStore({
+            host: config.redis.host,
+            port: config.redis.port,
+            prefix: config.redis.prefix + process.env.NODE_ENV + "_",
+            password: config.redis.key
         })
     }));
 
-    app.use( express.logger('dev') );
-    app.use( express.compress() );
-    app.use( express.favicon() );
-    app.use( express.methodOverride() );
+    app.use(express.logger('dev'));
+    app.use(express.compress());
+    app.use(express.favicon());
+    app.use(express.methodOverride());
 
-    app.use( passport.initialize() );
-    app.use( passport.session() );
+    app.use(passport.initialize());
+    app.use(passport.session());
 
     // register middleware for security headers
-    initializeSecurity( app, config );
+    initializeSecurity(app, config);
 
-    app.use( app.router );
+    app.use(app.router);
 
-    app.set( 'views', __dirname + '/src/views' );
-    app.set( 'view engine', 'ejs' );
-    app.set( 'view options', {
+    app.set('views', __dirname + '/src/views');
+    app.set('view engine', 'ejs');
+    app.set('view options', {
         layout: false
     });
 
     // error handler, outputs json since that's usually
     // what comes out of this thing
-    app.use(function( err, req, res, next ) {
+    app.use(function(err, req, res, next) {
         console.log('Express error catch', err);
         res.json(500, {
             error: err.toString()
@@ -106,12 +109,11 @@ app.configure(function() {
 });
 
 // register application routes
-initializeRoutes( app );
+initializeRoutes(app);
 
 module.exports = app;
 
-// if (require.main == module) {
+
 app.listen(webPort, function() {
     console.log("Starting server on port " + webPort + " in " + config.environmentName + " mode");
 });
-// }
